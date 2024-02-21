018675df4b505f9ccb904c350c322cb99f8ad52e
F_thx <1764362678@qq.com>
2024-02-21 12:41:12 +0800

    444

--- main.py
+++ main.py
@@ -69,6 +69,8 @@ def display_log_file(log_file_path, text_widget):
                     text_widget.insert(tk.END, line, "ERROR")
                 elif "[FATAL]" in line:
                     text_widget.insert(tk.END, line, "ERROR")
+                elif "[Error" in line:
+                    text_widget.insert(tk.END, line, "ERROR")
                 else:
                     text_widget.insert(tk.END, line, "DEFAULT")
     else:
